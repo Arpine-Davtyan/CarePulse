b7b6a8731c36ad92daf22e959bd694b2574dc08a
Arpine-Davtyan <arpinedavtyan5@gmail.com>
2026-07-07 17:23:15 +0400

    Dashboard

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,9 @@
 'use server'
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils"
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -35,3 +36,56 @@ export const getAppointment = async (appointmentId: string) => {
         );
     }
 }
+
+export const getRecentAppointmentList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [
+                Query.orderDesc("$createdAt"),
+                Query.select([
+                    "*",
+                    "patient.*"
+                ])
+            ]
+        );
+
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        };
+
+        const counts = (appointments.documents as Appointment[]).reduce(
+            (acc, appointment) => {
+                switch (appointment.status) {
+                    case "scheduled":
+                        acc.scheduledCount++;
+                        break;
+                    case "pending":
+                        acc.pendingCount++;
+                        break;
+                    case "cancelled":
+                        acc.cancelledCount++;
+                        break;
+                }
+                return acc;
+            },
+            initialCounts
+        );
+
+        const data = {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents,
+        };
+
+        return parseStringify(data);
+    } catch (error) {
+        console.error(
+            "An error occurred while retrieving the recent appointments:",
+            error
+        );
+    }
+};
